7e64ecdfdbb429e349775db79e900384af289c39
sunny kumar <sunnykumar1516@gmail.com>
2025-03-21 23:09:29 +0000

    initial folders

---
+++ template.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+import logging
+
+PROJECTNAME = "rainPredictionProject"
+list_files =[
+    ".github/workflows/.gitkeep",
+    f"src/{PROJECTNAME}/__init__.py",
+    f"src/{PROJECTNAME}/components/__init__.py",
+    f"src/{PROJECTNAME}/utilities/__init__.py",
+    f"src/{PROJECTNAME}/utilities/common.py",
+    f"src/{PROJECTNAME}/configurations/__init__.py",
+    f"src/{PROJECTNAME}/configurations/config.py",
+    f"src/{PROJECTNAME}/entity/__init__.py",
+    f"src/{PROJECTNAME}/entity/projectEntity.py",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "research/research.ipynb"
+
+]
+
+for filepath in list_files:
+    filepath = Path(filepath)
+    dir,filename = os.path.split(filepath)
+    if dir!= "":
+        os.makedirs(dir,exist_ok=True)
+        logging.info(f">>>>>> creating directory {dir}")
+    if (not os.path.exists(filepath) ) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f">>>>>> creating file {filepath}")
